b21f4f24b985f138c18d67da8466b81d5f67085e
Antonio Melgar Gonzalez <amelgar@gamma16.aulas.gsyc.urjc.es>
2018-01-30 10:50:25 +0100

    Tabla de multiplicar

---
+++ multiplica.py
@@ -0,0 +1,6 @@
+for numero1 in list(range(1,11)):
+	print("Tabla del " + str(numero1) + ": ");
+	for numero2 in list(range(1,11)):
+		print(str(numero1)+ " * " + str(numero2) + " = " + str(numero1*numero2));
+
+
